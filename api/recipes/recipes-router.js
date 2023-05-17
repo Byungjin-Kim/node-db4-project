34cab86c6dc9db20012b768330a5470412441788
Byungjin-Kim <composerjins@gmail.com>
2023-05-17 16:46:36 -0500

    wip

--- api/recipes/recipes-router.js
+++ api/recipes/recipes-router.js
@@ -3,7 +3,7 @@ const Recipes = require('./recipes-model')
 
 const router = express.Router();
 
-router.get('/:recepie_id', (req, res, next) => {
+router.get('/:recipe_id', (req, res, next) => {
     Recipes.getRecipeById(req.params.recipe_id)
         .then(recipe => {
             res.status(200).json(recipe);
