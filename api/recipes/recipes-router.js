cb59056de25833e4e57298631e57ffbd3f55d3a2
Byungjin-Kim <composerjins@gmail.com>
2023-05-17 14:26:03 -0500

    wip

--- api/recipes/recipes-router.js
+++ api/recipes/recipes-router.js
@@ -1,10 +1,23 @@
 const express = require('express');
+const Recipes = require('./recipes-model')
 
 const router = express.Router();
 
-router.use('*', (req, res) => {
-    res.json({ api: 'up' })
-})
+router.get('/:recepie_id', (req, res, next) => {
+    Recipes.getRecipeById(req.params.recipe_id)
+        .then(recipe => {
+            res.status(200).json(recipe);
+        })
+        .catch(next); // our custom err handling middleware in server.js will trap this
+});
+
+router.use((err, req, res, next) => { // eslint-disable-line
+    res.status(500).json({
+        customerMessage: 'something went wrong inside the recipes router',
+        message: err.message,
+        stack: err.stack,
+    });
+});
 
 
 
--- api/server.js
+++ api/server.js
@@ -1,19 +1,15 @@
 const express = require('express');
-const helmet = require('helmet');
 const recipesRouter = require('./recipes/recipes-router');
 
 const server = express();
 
-server.use(helmet());
 server.use(express.json());
+
 server.use('/api/recipes', recipesRouter);
 
-server.use((err, req, res, next) => { // eslint-disable-line
-    res.status(500).json({
-        customerMessage: 'something went wrong inside the recipes router',
-        message: err.message,
-        stack: err.stack,
-    });
-});
+server.use('*', (req, res) => {
+    res.json({ api: 'up' })
+})
+
 
 module.exports = server;
